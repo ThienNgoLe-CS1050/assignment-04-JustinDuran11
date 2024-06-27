ef10022acc7ae8b56a0631c7fb2bc68964d7a658
JustinDuran11 <justinduran1105@gmail.com>
2024-06-26 19:23:17 -0600

    Assignment04

--- Temp_Demo.java
+++ Temp_Demo.java
@@ -7,6 +7,18 @@ public class Temp_Demo
 {
    public static void main(String[] args)
    {
+	  double usertemp;
+      double answer;
       
+      Scanner keyboard = new Scanner(System.in);
+      System.out.print("Enter in a Fahrenheit temperature: ");
+     
+      usertemp = keyboard.nextDouble();
+      
+      Temperature temp = new Temperature(usertemp);
+      
+      System.out.println("Fahrenheit " + temp.getFahrenheit());
+      System.out.println("Celsius " + temp.getCelsius());
+      System.out.println("Kelvin " + temp.getKelvin());
    }     
 }
--- Temperature.java
+++ Temperature.java
@@ -1,9 +1,24 @@
-/**
- * Temperature Class The Temperature class holds a temperature in 
- * Fahrenheit and provide methods to get the 
- * temperature in Fahrenheit, Celsius, and Kelvin. 
- */
 public class Temperature
 {
+   private double fahrenheit; private double celsius; private double kelvin; private double ftemp;
+  
+   public Temperature(double fahrenheit) {
+	   ftemp = fahrenheit;
+   }
+    
+   public void setFahrenheit(double fahrenheit) {
+	   ftemp = fahrenheit;
+   }
+      
+   public double getFahrenheit() {
+	   return ftemp;
+   }
    
+   public double getCelsius() {
+	   return (5/9) * (ftemp -32);
+   }
+   
+   public double getKelvin() {
+	   return ((5/9) * (ftemp -32)) + 273;
+   }
 }
